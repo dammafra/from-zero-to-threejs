31ab6efa8eceb2e53a91f77b02dd6621c4fb84eb
Francesco Dammacco <francescodammacco@live.it>
2025-11-29 16:13:15 +0100

    Fix Player slide

--- src/components/slides/12-Player.tsx
+++ src/components/slides/12-Player.tsx
@@ -4,11 +4,22 @@ import { SlideBody, SlideText } from '@components/SlideBody'
 export function Player(props: SlideProps) {
   return (
     <Slide title="Player" {...props}>
-      <SlideBody>
+      <SlideBody fontSize={0.25}>
         <SlideText>Personaggio stilizzato:</SlideText>
         <SlideText bullet>Cubo per il corpo</SlideText>
         <SlideText bullet>Tronco di cono per i piedi</SlideText>
         <SlideText bullet>Occhi rettangolari</SlideText>
+        <SlideText> </SlideText>
+        <SlideText>Per farlo utilizzeremo le trasformazioni:</SlideText>
+        <SlideText bullet bold>
+          Position
+        </SlideText>
+        <SlideText bullet bold>
+          Scale
+        </SlideText>
+        <SlideText bullet bold>
+          Rotation
+        </SlideText>
       </SlideBody>
     </Slide>
   )
