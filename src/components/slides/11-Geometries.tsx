b0d85b05d1ce25885b50579406adf000d29afdac
Francesco Dammacco <francescodammacco@live.it>
2025-11-30 23:00:06 +0100

    Refactor geometries slide

--- src/components/slides/11-Geometries.tsx
+++ src/components/slides/11-Geometries.tsx
@@ -1,12 +1,10 @@
 import { Hoverable } from '@components/helpers'
 import { Slide, type SlideProps } from '@components/Slide'
 import { SlideText } from '@components/SlideBody'
+import { useIsTouch } from '@hooks'
 import { animated, config, useSpring } from '@react-spring/three'
 import {
   Billboard,
-  Box,
-  Capsule,
-  Circle,
   Cone,
   Cylinder,
   Dodecahedron,
@@ -30,17 +28,18 @@ type GeometryItemProps = Omit<JSX.IntrinsicElements['group'], 'id'> & {
 }
 
 function GeometryItem({ children, id, ...props }: GeometryItemProps) {
-  const [visible, setVisible] = useState(false)
+  const isTouch = useIsTouch()
+  const [visible, setVisible] = useState(isTouch)
   const hoverSpring = useSpring({ scale: visible ? 1 : 0, config: config.wobbly })
 
   return (
     <Hoverable
       {...props}
       onPointerOver={() => setVisible(true)}
-      onPointerLeave={() => setVisible(false)}
+      onPointerLeave={() => setVisible(isTouch)}
     >
       {children}
-      <AnimatedBillboard position-y={0.7} {...hoverSpring}>
+      <AnimatedBillboard position-y={0.7} position-z={isTouch ? -0.3 : 0} {...hoverSpring}>
         <Plane scale={[1.5, 0.25, 1]} material-color="black" />
         <SlideText
           bold
@@ -65,75 +64,103 @@ export function Geometries(props: SlideProps) {
   return (
     <Slide title="Geometries" {...props}>
       <group scale={0.85}>
-        {/* Box */}
-        <GeometryItem id="BoxGeometry" position={[-3, 0.5, -0.5]}>
-          <Box scale={0.8} material={material} castShadow />
-        </GeometryItem>
+        <group position-y={0.5}>
+          {/* First row */}
+          <group position-z={-0.5}>
+            {/* Box */}
+            <GeometryItem id="BoxGeometry" position-x={-3.2}>
+              <mesh scale={0.8} material={material} castShadow position-y={-0.1}>
+                <boxGeometry />
+              </mesh>
+            </GeometryItem>
 
-        {/* Capsule */}
-        <GeometryItem id="CapsuleGeometry" position={[-1.5, 0.6, -0.5]}>
-          <Capsule scale={0.4} material={material} castShadow />
-        </GeometryItem>
+            {/* Sphere */}
+            <GeometryItem id="SphereGeometry" position-x={-1.6}>
+              <Sphere scale={0.5} material={material} castShadow />
+            </GeometryItem>
 
-        {/* Circle */}
-        <GeometryItem id="CircleGeometry" position={[0, 0, -0.5]}>
-          <Circle scale={0.5} material={material} rotation-x={MathUtils.degToRad(-90)} />
-        </GeometryItem>
+            {/* Torus */}
+            <GeometryItem id="TorusGeometry">
+              <Torus
+                scale={0.35}
+                material={material}
+                rotation-x={MathUtils.degToRad(-90)}
+                castShadow
+                position-y={-0.36}
+              />
+            </GeometryItem>
 
-        {/* Cone */}
-        <GeometryItem id="ConeGeometry" position={[1.5, 0.5, -0.5]}>
-          <Cone scale={[0.5, 1, 0.5]} material={material} castShadow />
-        </GeometryItem>
+            {/* TorusKnot */}
+            <GeometryItem id="TorusKnotGeometry" position-x={1.6}>
+              <TorusKnot
+                scale={0.3}
+                material={material}
+                rotation-x={MathUtils.degToRad(-90)}
+                position-y={-0.23}
+              />
+            </GeometryItem>
 
-        {/* Cylinder */}
-        <GeometryItem id="CylinderGeometry" position={[3, 0.5, -0.5]}>
-          <Cylinder scale={[0.5, 1, 0.5]} material={material} castShadow />
-        </GeometryItem>
+            {/* Cylinder */}
+            <GeometryItem id="CylinderGeometry" position-x={3.2}>
+              <Cylinder scale={[0.5, 1, 0.5]} material={material} castShadow />
+            </GeometryItem>
+          </group>
 
-        {/* Dodecahedron */}
-        <GeometryItem id="DodecahedronGeometry" position={[-3, 0.5, 0.7]}>
-          <Dodecahedron scale={0.5} material={material} castShadow />
-        </GeometryItem>
+          {/* Second row */}
+          <group position-z={0.7}>
+            {/* Plane */}
+            <GeometryItem id="PlaneGeometry" position-x={-3.2}>
+              <mesh material={material} rotation-x={MathUtils.degToRad(-90)} position-y={-0.5}>
+                <planeGeometry args={[0.8, 0.8]} />
+              </mesh>
+            </GeometryItem>
 
-        {/* Icosahedron */}
-        <GeometryItem id="IcosahedronGeometry" position={[-1.5, 0.5, 0.7]}>
-          <Icosahedron scale={0.5} material={material} castShadow />
-        </GeometryItem>
+            {/* Circle */}
+            <GeometryItem id="CircleGeometry" position-x={-1.6}>
+              <mesh material={material} rotation-x={MathUtils.degToRad(-90)} position-y={-0.5}>
+                <circleGeometry args={[0.5]} />
+              </mesh>
+            </GeometryItem>
 
-        {/* Plane */}
-        <GeometryItem id="PlaneGeometry" position={[0, 0, 0.7]}>
-          <Plane scale={0.8} material={material} rotation-x={MathUtils.degToRad(-90)} />
-        </GeometryItem>
+            {/* Ring */}
+            <GeometryItem id="RingGeometry">
+              <Ring
+                scale={0.5}
+                material={material}
+                rotation-x={MathUtils.degToRad(-90)}
+                position-y={-0.5}
+              />
+            </GeometryItem>
 
-        {/* Octahedron */}
-        <GeometryItem id="OctahedronGeometry" position={[1.5, 0.5, 0.7]}>
-          <Octahedron scale={0.5} material={material} castShadow />
-        </GeometryItem>
+            {/* Capsule */}
+            <GeometryItem id="CapsuleGeometry" position-x={1.6}>
+              <mesh material={material} castShadow position-y={0.05}>
+                <capsuleGeometry args={[0.4, 0.3]} />
+              </mesh>
+            </GeometryItem>
 
-        {/* Ring */}
-        <GeometryItem id="RingGeometry" position={[3, 0, 0.7]}>
-          <Ring scale={0.5} material={material} rotation-x={MathUtils.degToRad(-90)} />
-        </GeometryItem>
+            {/* Cone (swapped with TorusKnot) */}
+            <GeometryItem id="ConeGeometry" position-x={3.2}>
+              <Cone scale={[0.5, 1, 0.5]} material={material} castShadow />
+            </GeometryItem>
+          </group>
 
-        {/* Sphere */}
-        <GeometryItem id="SphereGeometry" position={[-2.25, 0.5, 2]}>
-          <Sphere scale={0.5} material={material} castShadow />
-        </GeometryItem>
-
-        {/* Tetrahedron */}
-        <GeometryItem id="TetrahedronGeometry" position={[-0.75, 0.4, 2]}>
-          <Tetrahedron scale={0.6} material={material} castShadow />
-        </GeometryItem>
-
-        {/* Torus */}
-        <GeometryItem id="TorusGeometry" position={[0.75, 0.2, 2]}>
-          <Torus scale={0.4} material={material} rotation-x={MathUtils.degToRad(-90)} castShadow />
-        </GeometryItem>
-
-        {/* TorusKnot */}
-        <GeometryItem id="TorusKnotGeometry" position={[2.25, 0.4, 2]}>
-          <TorusKnot scale={0.35} material={material} rotation-x={MathUtils.degToRad(-90)} />
-        </GeometryItem>
+          {/* Third row */}
+          <group position-z={2}>
+            <GeometryItem id="TetrahedronGeometry" position-x={-2.4}>
+              <Tetrahedron scale={0.6} material={material} castShadow position-y={-0.15} />
+            </GeometryItem>
+            <GeometryItem id="OctahedronGeometry" position-x={-0.8}>
+              <Octahedron scale={0.5} material={material} castShadow />
+            </GeometryItem>
+            <GeometryItem id="IcosahedronGeometry" position-x={0.8}>
+              <Icosahedron scale={0.5} material={material} castShadow position-y={-0.07} />
+            </GeometryItem>
+            <GeometryItem id="DodecahedronGeometry" position-x={2.4}>
+              <Dodecahedron scale={0.5} material={material} castShadow position-y={-0.03} />
+            </GeometryItem>
+          </group>
+        </group>
       </group>
     </Slide>
   )
