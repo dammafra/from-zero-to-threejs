8d25cdb0bfb0ce15ac726345717c3608b3bd90c3
Francesco Dammacco <francescodammacco@live.it>
2025-12-01 00:43:30 +0100

    Fix presentation controls

--- src/components/PresentationControls.tsx
+++ src/components/PresentationControls.tsx
@@ -1,18 +1,18 @@
 import { Icon } from '@iconify/react'
 
 export function PresentationControls() {
-  const emulatePressKey = (key: string) => {
-    dispatchEvent(new KeyboardEvent('keydown', { key }))
-    setTimeout(() => dispatchEvent(new KeyboardEvent('keyup', { key })))
+  const emulatePressKey = (code: string) => {
+    dispatchEvent(new KeyboardEvent('keydown', { code }))
+    setTimeout(() => dispatchEvent(new KeyboardEvent('keyup', { code })))
   }
 
   return (
     <div className="controls">
-      <button title="Home" onClick={() => emulatePressKey('KeyH')}>
-        <Icon icon="icon-park-twotone:circle-double-left" />
-      </button>
       <button title="Reset" onClick={() => emulatePressKey('KeyR')}>
         <Icon icon="icon-park-twotone:radio-two" />
+      </button>
+      <button title="Home" onClick={() => emulatePressKey('KeyH')}>
+        <Icon icon="icon-park-twotone:circle-double-left" />
       </button>
       <button title="Previous" onClick={() => emulatePressKey('ArrowUp')}>
         <Icon icon="icon-park-twotone:left-c" />
