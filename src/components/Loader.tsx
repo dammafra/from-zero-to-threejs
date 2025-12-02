9ea5e1a0c79793df475b38eeef5025fce84380f0
Francesco Dammacco <francescodammacco@live.it>
2025-12-02 01:32:39 +0100

    Improve loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,18 +1,10 @@
+import { Billboard, Html } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import { useEffect, useRef } from 'react'
+import { useRef } from 'react'
 import { Object3D } from 'three'
 
 export function Loader() {
   const ref = useRef<Object3D>(null)
-
-  useEffect(() => {
-    const loader = document.getElementById('loader')
-    if (!loader) return
-
-    loader.style.visibility = 'visible'
-
-    return () => loader.remove()
-  }, [])
 
   useFrame((_, delta) => {
     if (!ref.current) return
@@ -20,9 +12,16 @@ export function Loader() {
   })
 
   return (
-    <mesh ref={ref}>
-      <boxGeometry />
-      <meshStandardMaterial color="orange" />
-    </mesh>
+    <>
+      <mesh ref={ref}>
+        <boxGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+      <Billboard position-y={-1.5}>
+        <Html transform className="text-lg font-title text-white">
+          Loading
+        </Html>
+      </Billboard>
+    </>
   )
 }
