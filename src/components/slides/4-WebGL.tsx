58efd0d98c4733268e99d2489f0a7c4d68e1f8ba
Francesco Dammacco <francescodammacco@live.it>
2025-12-06 16:19:47 +0100

    Improve WebGL logo animation

--- src/components/slides/4-WebGL.tsx
+++ src/components/slides/4-WebGL.tsx
@@ -1,8 +1,13 @@
 import { Slide, type SlideProps } from '@components'
 import { WebGLLogo } from '@components/models'
+import { animated } from '@react-spring/three'
+import { useSpring } from '@react-spring/web'
 import { Billboard, Center, Float } from '@react-three/drei'
 import { useOverlay } from '@stores'
 import { useEffect } from 'react'
+import type { Vector3Tuple } from 'three'
+
+const AnimatedBillboard = animated(Billboard)
 
 export function WebGL(props: SlideProps) {
   const setLogo = useOverlay(s => s.setLogo)
@@ -11,15 +16,20 @@ export function WebGL(props: SlideProps) {
     setLogo({ position: [-2, 0, 0], scale: 0.5 })
   }, [setLogo])
 
+  const spring = useSpring({
+    from: { position: [20, 0, 0] as Vector3Tuple },
+    to: { position: [2, 0, 0] as Vector3Tuple },
+  })
+
   return (
     <Slide background={false} {...props}>
-      <Billboard position-x={2}>
+      <AnimatedBillboard position={spring.position}>
         <Float speed={1.5}>
           <Center scale={2}>
             <WebGLLogo />
           </Center>
         </Float>
-      </Billboard>
+      </AnimatedBillboard>
     </Slide>
   )
 }
